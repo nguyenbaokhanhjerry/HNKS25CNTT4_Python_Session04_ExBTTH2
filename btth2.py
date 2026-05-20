536fa4867a3118c3d63e0be52441867de11d0035
nguyenbaokhanhjerry <khanhbaonguyen76@gmail.com>
2026-05-20 21:05:09 +0700

    Update btth2.py

--- btth2.py
+++ btth2.py
@@ -1,17 +1,14 @@
-total_revenue = 0
-revenue_days = 0
+total = 0
+count = 0
 
-for day in range(1, 8, 1):
-    revenue = int(input(f"Nhập doanh thu Ngày {day}: "))
+for day in range(7):
+    revenue = int(input("Nhập doanh thu: "))
 
-    total_revenue += revenue
+    total = total + revenue
 
     if revenue >= 5000000:
-        revenue_days += 1
+        count = count + 1
 
-average = total_revenue / 7
-
-print("\n--- BÁO CÁO DOANH THU TUẦN ---")
-print("Tổng doanh thu:", total_revenue)
-print("Trung bình mỗi ngày:", average)
-print("Số ngày đạt mục tiêu:", revenue_days)
+print("Tổng doanh thu:", total)
+print("Trung bình:", total / 7)
+print("Số ngày đạt mục tiêu:", count)
